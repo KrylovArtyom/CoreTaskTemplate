c3f20e517e745e59771a5db8ddacdf581b5c30fb
KrylovArtyom <88318874+KrylovArtyom@users.noreply.github.com>
2021-08-04 19:59:07 +0300

    настройка соединения с БД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+	// реализуйте настройку соеденения с БД
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String HOSTNAME = "localhost";
+	private static final String DBNAME = "java_project";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "sdf23mSF_13421";
+	private static final String URL = "jdbc:mysql://" + HOSTNAME + ":3306/" + DBNAME;
+
+	public static Connection getMySQLConnection() {
+		Connection connection = null;
+		try{
+			Class.forName(DRIVER);
+			System.out.println("драйвер подключен");
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			System.out.println("Соелинение установлено");
+		} catch (SQLException e) {
+			System.out.println("Проверь с отображением БД");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Проблема с JDBC");
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
 }
